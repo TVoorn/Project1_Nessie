016b2e1b63829c4a066eecb562e5d5ca09d78402
Tjarda112 <tjardavoorn@hotmail.com>
2016-10-12 13:45:34 +0200

    CVS root mapping

--- connect.php
+++ connect.php
@@ -9,13 +9,27 @@
 $inputuser = $_POST['user'];
 $inputpass = $_POST['pass'];
 
+
 $user = "";
 $password = "";
 
 $connect = mysqli_connect("localhost",$Username,$Password,$Nessie_users)
 or die('Error connecting to MySQL server.');
 
-$query
+$query = "SELECT * FROM 'users' WHERE 'user' = '$inputuser'";
+$querypass = "SELECT * FROM 'users' WHERE 'password' = 'inputpass' ";
 
+$result = mysqli_query($query);
+$resultpass = mysqli_query($querypass);
 
+$row = mysqli_fetch_array($result);
+$rowpass = mysqli_fetch_array($resultpass);
+
+$serveruser = $row["username"];
+$serverpass = $row["password"];
+
+echo $result;
+echo $resultpass;
+
+mysqli_close();
 ?>
