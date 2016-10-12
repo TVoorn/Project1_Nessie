8a286fdaa292eb7b78cc50eb68a77c0c5257f62a
Tjarda112 <tjardavoorn@hotmail.com>
2016-10-12 12:30:53 +0200

    Git connecting check phpstorm

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tjardavoorn
+ * Date: 12/10/2016
+ * Time: 11:07
+ */
+
+$inputuser = $_POST['user'];
+$inputpass = $_POST['pass'];
+
+$user = "";
+$password = "";
+
+$connect = mysqli_connect("localhost",$Username,$Password,$Nessie_users)
+or die('Error connecting to MySQL server.');
+
+$query
+
+
+?>
