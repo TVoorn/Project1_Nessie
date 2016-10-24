fea5fda6567cc6dbf8e24a3be8b0042492cde58d
Tjarda112 <tjardavoorn@hotmail.com>
2016-10-24 10:16:23 +0200

    added Recaptha to the nessie website

--- connect.php
+++ connect.php
@@ -10,14 +10,40 @@ $inputuser = $_POST['user'];
 $inputpass = $_POST['pass'];
 
 
-$user = "";
-$password = "";
+$user = 'root';
+$password = 'root';
+$db = 'database_test';
+$host = 'localhost';
+$port = 8889;
 
-$connect = mysqli_connect("localhost",$Username,$Password,$Nessie_users)
-or die('Error connecting to MySQL server.');
+$link = mysqli_init(); // <- gebruiken voor queries
+$success = mysqli_real_connect(
+    $link,
+    $host,
+    $user,
+    $password,
+    $db,
+    $port
+) or die("Database not connected correctly");
+
+$result = $link->query("SELECT * FROM account WHERE username='12344'") or die($link->error);
+
+print_r($result);
+
+
+
+/*
+
+$sql = "SELECT * FROM 'username' WHERE 'username' = 'test'";
+
+
+$results = mysqli_query($sql);
+var_dump($results);
+
+
 
 $query = "SELECT * FROM 'users' WHERE 'user' = '$inputuser'";
-$querypass = "SELECT * FROM 'users' WHERE 'password' = 'inputpass' ";
+$querypass = "SELECT * FROM 'users' WHERE 'password' = '$inputpass' ";
 
 $result = mysqli_query($query);
 $resultpass = mysqli_query($querypass);
@@ -28,8 +54,16 @@ $rowpass = mysqli_fetch_array($resultpass);
 $serveruser = $row["username"];
 $serverpass = $row["password"];
 
+if($serveruser&&$serveruser){
+    if(!$result){
+        die("Username or password is invalid");
+    }
+}
+
 echo $result;
 echo $resultpass;
 
 mysqli_close();
+
+*/
 ?>
