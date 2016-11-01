2e912cc0a1d564daa1fcb3d096e1444e880707df
Tjarda112 <tjardavoorn@hotmail.com>
2016-11-01 11:19:59 +0100

    login error message added

--- includes/login.php
+++ includes/login.php
@@ -10,8 +10,11 @@ $password = trim($_POST['password']);
 $sql = "SELECT * FROM account WHERE username='$username' AND password='$password' ";
 $result = mysqli_query($link, $sql);
 
+
 if(!$row = mysqli_fetch_assoc($result)){
-    echo "Your username or password is incorrect!";
+
+    die(header("location:../index.php?loginFailed=true&reason=password"));
+
 } else{
     $_SESSION['id'] = $row['id'];
 
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ session_start();
 require 'connect.php';
 require 'security.php';
 require 'includes/register.php';
+
+
 ?>
 
 <!DOCTYPE html>
@@ -36,12 +38,17 @@ require 'includes/register.php';
         </form>
         <form class="login-form" action="includes/login.php" method="post">
 
+
             <input type="text" name="username" id="username" placeholder="username" autocomplete="off" required/>
             <input type="password" name="password" id="password" placeholder="password" autocomplete="off" required/>
             <button>login</button>
+            <div class="login-error">
+            <?php $reasons = array("password" => "Wrong Username or Password", "blank" => "You have left one or more fields blank."); if ($_GET["loginFailed"])
+                echo $reasons[$_GET["reason"]]; ?>
+    </div>
             <p class="message">Not registered? <a href="#">Create an account</a></p>
         </form>
-
+<!--
 
         <form action="includes/logout.php">
             <button>LOG OUT</button>
@@ -55,7 +62,7 @@ require 'includes/register.php';
         }
 
         ?>
-
+-->
     </div>
 </div>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
